f96c4d4b7c44f47f6a29bcf1bf4d69ac191a1fb9
muskanverma1234 <musuladdu@gmail.com>
2024-03-13 22:46:56 +0530

    add operators file to py-program

--- operators.py
+++ operators.py
@@ -27,3 +27,71 @@
     (is , is not)
 '''
 
+#arithmetic operator
+a=10
+b=5
+print("addition is:",a+b)
+print("substraction is:",a-b)
+print("multiplication is:",a*b)
+print("division is:",a/b)
+print(" reminderis:",a%b)
+print("exponent is:",a**b)
+print("float value is:",a//b)
+
+#assignment operator
+print("a==b",a==b)
+print("a+=b",a+b)
+print("a-=b",a-b)
+print("a*=b",a*b)
+print("a/b",a/b)
+print("a**b",a**b)
+print("a//b",a//b)
+
+#comparision operator
+print("two numbers are equal or not",a==b)
+print("two numbers are not equal or not",a!=b)
+print("a is less then or equal to b",a<=b)
+print("a is greater than or equal to b",a>=b)
+print("a is greater than b",a>b)
+print("a is less than b",a<b)
+
+#bitwise operator
+print("a&b",a&b)
+print("a\b",a\b)
+print("a^b",a^b)
+print("~a",~a)
+print("a<<b",a<<b)
+print("a>>b",a>>b)
+
+#logical operator
+print("is this statement true",a>3 and a<5)
+print("any one statement is true",a>3 or a<5)
+print("each statement is true then return false and vice-versa",(not(a>3 and a<5)))
+
+#membership operator
+x=["rose","lotus"]
+print("is value present in x", "rose" in x)
+print("is value not present in x","rupal" not in x)
+
+#identity operator
+s=["rose","lotus"]
+r=["rose","lotus"]
+p=s
+print(a is p)
+print(a is not p)
+print(a is b)
+print(a is not b)
+print(a==b)
+print(a!=b)
+
+
+
+
+
+
+
+
+
+
+
+
