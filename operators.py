63ec4d7e78b278d7b3cece30c01bc7378ee75be7
muskanverma1234 <musuladdu@gmail.com>
2024-03-13 22:17:56 +0530

    add operators file to py-program

---
+++ operators.py
@@ -0,0 +1,29 @@
+''' 
+    operators - operators are used to perform operation on two operands .
+    for example - a+b , here a and b are two operands and + is an operator.
+
+    types of operators :
+
+    1.arithmetic operators - they are used to perfrom arithmetic operation on operands.
+    (+,-,*,/,%,**,//)
+
+    2.assignment operators - they are used to assign values to the variables.
+    (=,+=,-=,*=,/=,%=,**=,//=)
+
+    3.comparision operator - they are used to compare value of two operands.
+    (==,!=,<=,>=,<,>)
+
+    4.bitwise operator - two operands value are processed bit by bit by this operator.
+    (&,\,^,~,<<,>>)
+
+    5.logical operator - the assessment of expression to make decision.
+    (AND,OR,NOT)
+
+    6.membership operator - the membership of a value inside a python data structure can be verified
+                         using python membership operator.
+    (in,not in)
+
+    7.identity operator - they are used to check that the item exist or not.
+    (is , is not)
+'''
+
