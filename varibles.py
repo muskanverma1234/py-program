4f9bd6fa86c80ed6dc334444b26f6b6ffa42bb65
muskanverma1234 <musuladdu@gmail.com>
2024-03-13 21:34:15 +0530

    add my new file to git

---
+++ varibles.py
@@ -0,0 +1,14 @@
+# rules for identifier....
+""" 
+1. an identifier name must contain alplabets ,numerals and underscore.
+2. an identifier name must be started within alphabets or underscore.
+3. an identifier name must not be started with an numeral.
+4. the length of an identifier name must not be greater then 50 characters.
+5. your identifier name should be connected to entity.
+"""
+
+Name = "muskan verma"
+Name_of_course = "BCA 3rd year"
+
+print(Name)
+print(Name_of_course)
