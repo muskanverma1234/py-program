4b5a2c622b67f9b15d27726d208e7fb135874755
muskanverma1234 <musuladdu@gmail.com>
2024-03-18 21:20:08 +0530

    update my new file

--- tuple.py
+++ tuple.py
@@ -10,3 +10,39 @@ Tuples are an immutable data type, meaning their elements cannot be changed afte
 Each element in a tuple has a specific order that will never change because tuples are ordered 
 sequences.
 '''
+
+#tuple creation
+tup1 = (1,2,3,4)
+print(tup1)
+
+#tuple indexing
+print(tup1[3])
+
+#tuple slicing
+print(tup1[1:3])
+
+#tuple repetation
+tup1 = tup1 * 3
+print(tup1)
+
+#tuple count()
+tupl = tup1.count(2)
+print(tupl)
+
+# membership operator
+print(2 in tup1)
+print(6 in tup1)
+
+# iterating over tuple
+t1 = ("python","tuple","ordered","immutable")
+for i in t1:
+    print(i)
+    
+output : 
+(1,2,3,4)
+(4)
+(2,3,4)
+(1,2,3,4,1,2,3,4,1,2,3,4)
+1
+true
+false
