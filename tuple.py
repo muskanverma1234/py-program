d9734c55785fe1e41238407413719892681e7230
muskanverma1234 <musuladdu@gmail.com>
2024-03-18 20:51:43 +0530

    update my new file

---
+++ tuple.py
@@ -0,0 +1,12 @@
+# TUPLES 
+'''
+A comma-separated group of items is called a Python triple. The ordering, settled items, and 
+reiterations of a tuple are to some degree like those of a rundown, but in contrast to a rundown, 
+a tuple is unchanging.
+'''
+'''
+Features of Python Tuple
+Tuples are an immutable data type, meaning their elements cannot be changed after they are generated.
+Each element in a tuple has a specific order that will never change because tuples are ordered 
+sequences.
+'''
