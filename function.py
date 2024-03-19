88981661a6d6af62d17e369ae7a8aaf2ae88a969
muskanverma1234 <musuladdu@gmail.com>
2024-03-19 22:41:22 +0530

    update function.py

--- function.py
+++ function.py
@@ -21,4 +21,30 @@ print("square of given number is",s1)
 
 output : square of given number is 36
 
+'''
+two types of functions :
+1.built-in functions : The Python built-in functions are defined as the functions whose functionality 
+                       is pre-defined in Python. The python interpreter has several functions that are
+                        always present for use. These functions are known as Built-in Functions. 
+                        There are several built-in functions in Python which are listed below:
+
+float() -it returns the floating point number form a number or string.
+print(float(9)) --> 0.9
+
+sum() - used to get the sum of numbers of an iterable i,e. list
+p=sum([1,4,5])
+print(p) --> 10
+
+len() - the length function is used to return the length of an object.
+str = "muskan"
+print(len(str)) --> 6
+
+min() - it is used to find out the smallest number from the given collection.
+m = [2,5,8]
+print(min(m)) --> 2
+
+pow() - it is used to calculate the power of given number.
+print(pow(2,3)) --> 8
+
+
 
