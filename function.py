76acedb12589a15274e58d67500b161da3b40790
muskanverma1234 <musuladdu@gmail.com>
2024-03-19 22:22:57 +0530

    update function.py

---
+++ function.py
@@ -0,0 +1,24 @@
+'''
+Function - A collection of related assertions that carry out a mathematical, analytical, or evaluative
+           operation is known as a function. 
+advantages of function :
+1. Once defined, Python functions can be called multiple times and from any location in a program.
+2.Our Python program can be broken up into numerous, easy-to-follow functions if it is significant.
+3.The ability to return as many outputs as we want using a variety of arguments is one of Python's most
+ significant achievements.
+4.However, Python programs have always incurred overhead when calling functions.
+
+syntax:
+def function_name(parameters):
+    code block
+'''
+
+#creating function 
+def square():
+    return num**2
+s1=square(6)
+print("square of given number is",s1)
+
+output : square of given number is 36
+
+
